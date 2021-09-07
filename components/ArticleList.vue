ebf867412f71800de815cd3abafb5d4ec636f5a2
橋本光司 <hashimoto-kouji@chuo-computer.co.jp>
2021-09-07 15:40:04 +0900

    article list

---
+++ components/ArticleList.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt-link :to="slug">
+    <div class="border p-4 rounded my-1">
+      <h2 class="text-2xl mb-2">{{ title }}</h2>
+      <div class="text-l text-gray-400"><code>/{{ slug }}</code></div>
+      <div class="text-l">{{ date }}</div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    date: { type: String, required: false, default: 'xxxx.xx.xx' },
+    slug: { type: String, required: true },
+  },
+}
+</script>
